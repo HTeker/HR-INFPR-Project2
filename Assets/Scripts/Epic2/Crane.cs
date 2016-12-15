a503172f77c0e8eaf3022de2bdbd5f443a3cf0e6
Halil Teker <h.teker@evision-software.com>
2016-12-15 16:51:34 +0100

    Tussentijdse commit

---
+++ Assets/Scripts/Epic2/Crane.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crane : MonoBehaviour {
+
+    public GameObject hook;
+    public float moveSpeed = 25f;
+
+    // Update is called once per frame
+    void Update () {
+        if(transform.position.z <= 115 && transform.position.z >= 0)
+        {
+            float vertical = Input.GetAxis("Vertical");
+            transform.Translate(moveSpeed * -vertical * Time.deltaTime, 0f, 0f);
+        }
+        else
+        {
+            if(transform.position.z > 115)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 115);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            }
+            
+        }
+        
+	}
+    
+}
---
+++ Assets/Scripts/Epic2/Hook.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hook : MonoBehaviour {
+
+    public float moveSpeed = 25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(0f, 0f, moveSpeed * horizontal * Time.deltaTime);
+    }
+}

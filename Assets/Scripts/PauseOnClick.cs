5b2256055b3fc3c85ecb70da5533990444d70139
Halil Teker <h.teker@evision-software.com>
2017-01-14 00:41:39 +0100

    no message

---
+++ Assets/Scripts/PauseOnClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseOnClick : MonoBehaviour {
+
+    bool paused = false;
+
+    void OnGUI()
+    {
+        if(paused)
+        {
+            GUILayout.Label("Game is paused!");
+            if (GUILayout.Button("Click me to unpaue"))
+                //paused = togglePause();
+                togglePause();
+        }
+    }
+
+    public void togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            //return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+           // return (true);
+        }
+    }
+}

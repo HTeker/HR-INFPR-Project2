46a52d5dedbdb2b6da2153e6177294fe8383ef8c
MrRedTube <mathijsparie@gmail.com>
2017-01-15 20:11:03 +0100

    Help screen WIP

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,8 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
 
+    public PanelController canvasPanelController;
+    public PanelController helpScreenPanelController;
+
+    public void Start()
+    {
+        canvasPanelController.SwitchToPanelByName("Panel_MainMenu");
+    }
+
 	public void LoadScene(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void LoadHelpScreen(string helpScreenName)
+    {
+        canvasPanelController.SwitchToPanelByName("Panel_HelpScreen");
+        helpScreenPanelController.SwitchToPanelByName("Panel_" + helpScreenName);
     }
 }
---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour {
+
+    public GameObject[] panels;
+
+    public void SwitchToPanelByName(string panelName)
+    {
+        foreach(GameObject panel in panels)
+        {
+            if (panel.name == panelName)
+                panel.SetActive(true);
+            else
+                panel.SetActive(false);
+        }
+    }
+}
